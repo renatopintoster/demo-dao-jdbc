87079d9f5494eb0114496ba4f072f2c87f9b10a4
Renato Cavalcanti <renatopintoster@gmail.com>
2026-05-07 20:03:14 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
@@ -8,6 +11,7 @@ public class Program {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
 
-
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
